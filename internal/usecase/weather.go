12eb89957fee9b687eba2086dc82c500b5752d8e
pavel <pavel_solovev21@mail.ru>
2024-03-29 21:25:34 +0300

    fix: change method of humidity and pressure calculation

--- internal/usecase/weather.go
+++ internal/usecase/weather.go
@@ -83,19 +83,23 @@ func calcTemp(parsedResult *weather.WeatherData, respData *weather.ResponseData)
 func calcHumidity(parsedResult *weather.WeatherData, respData *weather.ResponseData) {
 	var avgVal float64
 	var valuesMedian []float64
+	respData.Humidity.Min = 100
+	respData.Humidity.Max = 0
 
-	for i, day := range parsedResult.Days {
-		if i == 0 || day.Humidity < respData.Humidity.Min {
-			respData.Humidity.Min = day.Humidity
+	for _, day := range parsedResult.Days {
+		for _, hour := range day.Hours {
+			if hour.Humidity < respData.Humidity.Min {
+				respData.Humidity.Min = hour.Humidity
+			}
+			if hour.Humidity > respData.Humidity.Max {
+				respData.Humidity.Max = hour.Humidity
+			}
+			avgVal += hour.Humidity
+			valuesMedian = append(valuesMedian, hour.Humidity)
 		}
-		if i == 0 || day.Humidity > respData.Humidity.Max {
-			respData.Humidity.Max = day.Humidity
-		}
-		avgVal += day.Humidity
-		valuesMedian = append(valuesMedian, day.Humidity)
 	}
 
-	respData.Humidity.Average = avgVal / float64(len(parsedResult.Days))
+	respData.Humidity.Average = avgVal / float64(len(parsedResult.Days)*24)
 
 	if len(valuesMedian) > 0 {
 		respData.Humidity.Median = calculateMedian(valuesMedian)
@@ -105,19 +109,23 @@ func calcHumidity(parsedResult *weather.WeatherData, respData *weather.ResponseD
 func calcPressure(parsedResult *weather.WeatherData, respData *weather.ResponseData) {
 	var avgVal float64
 	var valuesMedian []float64
+	respData.PressureMb.Min = 2000
+	respData.PressureMb.Max = 0
 
-	for i, day := range parsedResult.Days {
-		if i == 0 || day.Pressure < respData.PressureMb.Min {
-			respData.PressureMb.Min = day.Pressure
+	for _, day := range parsedResult.Days {
+		for _, hour := range day.Hours {
+			if hour.Pressure < respData.PressureMb.Min {
+				respData.PressureMb.Min = hour.Pressure
+			}
+			if hour.Pressure > respData.PressureMb.Max {
+				respData.PressureMb.Max = hour.Pressure
+			}
+			avgVal += hour.Pressure
+			valuesMedian = append(valuesMedian, hour.Pressure)
 		}
-		if i == 0 || day.Pressure > respData.PressureMb.Max {
-			respData.PressureMb.Max = day.Pressure
-		}
-		avgVal += day.Pressure
-		valuesMedian = append(valuesMedian, day.Pressure)
 	}
 
-	respData.PressureMb.Average = avgVal / float64(len(parsedResult.Days))
+	respData.PressureMb.Average = avgVal / float64(len(parsedResult.Days)*24)
 
 	if len(valuesMedian) > 0 {
 		respData.PressureMb.Median = calculateMedian(valuesMedian)
