142263e5a6595b4f028ab5287fef5d1a5fd8854a
pavel <pavel_solovev21@mail.ru>
2024-04-04 01:12:37 +0300

    fix: change keyboard init

--- internal/usecase/telegram/tg_req.go
+++ internal/usecase/telegram/tg_req.go
@@ -35,18 +35,24 @@ var _ usecase.TelegramReq = (*TelegramBot)(nil)
 func (t TelegramBot) InitRequest(data gitlab.WebhookData) (*http.Request, error) {
 	log.Println("init request")
 	message := fmt.Sprintf("Issue #%d\nStudent: %s\nStatus: %s → %s\n", data.IssueNumber, data.StudentRepoName, data.PreviousStatus, data.NewStatus)
-	log.Println(message)
 
-	var ikm ikMarkup
-	ikm.InlineKeyboard = append(ikm.InlineKeyboard, []ik{})
-	ikm.InlineKeyboard[0] = append(ikm.InlineKeyboard[0], ik{}, ik{})
+	//var ikm ikMarkup
+	//ikm.InlineKeyboard = append(ikm.InlineKeyboard, []ik{})
+	//ikm.InlineKeyboard[0] = append(ikm.InlineKeyboard[0], ik{}, ik{})
+	//
+	//ikm.InlineKeyboard[0][0].Text = "Issue"
+	//ikm.InlineKeyboard[0][0].Url = data.IssueURL
+	//ikm.InlineKeyboard[0][1].Text = "Repo"
+	//ikm.InlineKeyboard[0][1].Url = data.RepoURL
 
-	ikm.InlineKeyboard[0][0].Text = "Issue"
-	ikm.InlineKeyboard[0][0].Url = data.IssueURL
-	ikm.InlineKeyboard[0][1].Text = "Repo"
-	ikm.InlineKeyboard[0][1].Url = data.RepoURL
+	kb := &ikMarkup{InlineKeyboard: [][]ik{
+		{
+			{Text: "Issue", Url: data.IssueURL},
+			{Text: "Repo", Url: data.RepoURL},
+		},
+	}}
 
-	marshalled, err := json.Marshal(ikm)
+	marshalled, err := json.Marshal(kb)
 	if err != nil {
 		log.Printf("Error in marshalling buttons struct")
 		return nil, err
